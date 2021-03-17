d327c5bf3f74d26fe2b9bf8c61f2be3cd2dafe4e
Jake <jakekippes123gmail.com>
2021-03-16 21:23:12 -0500

    fixing import on react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CharacterCreator from "./pages/CharacterCreator";
 import Login from "./pages/Login";
 import CharacterList from "./pages/CharacterList";
