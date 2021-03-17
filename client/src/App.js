3b03f0b49ae56652b496aed7165e4b3014d4f4fe
Christian Goldman <christian.goldman9638@gmail.com>
2021-03-16 20:15:27 -0500

    reorganize file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import AbilityScores from "./components/AbilityScores/AbilityScores";
-import AttributeList from "./components/AttributeList/AttributeList";
-import CharacterBlock from "./components/CharacterBlock/CharacterBlock";
-
-const classes = ["Cleric", "Fighter", "Paladin", "Ranger", "Rogue", "Sorcerer"];
-const skills = [
-  "Acrobatics",
-  "Animal",
-  "Arcana",
-  "Athletics",
-  "Deception",
-  "History",
-  "Insight",
-  "Intimidation",
-  "Investigation",
-  "Medicine",
-  "Nature",
-  "Perception",
-  "Performance",
-  "Persuasion",
-  "Religion",
-  "Sleight",
-  "History",
-  "Stealth",
-  "Survival",
-];
-const race = [
-  "Human",
-  "Elf",
-  "Dwarf",
-  "Half",
-  "Gnome"]
+import CharacterCreator from "./components/CharacterCreator/CharacterCreator";
 function App() {
   return (
     <>
-      <Navbar />
-      <CharacterBlock />
-      <AttributeList attributes={ classes } title="Classes" />
-      <AttributeList attributes={ race } title="Race"/>
-      <AbilityScores />
-      <AttributeList attributes={ skills } title="Skills"/>
+      <CharacterCreator />
     </>
   );
 }
