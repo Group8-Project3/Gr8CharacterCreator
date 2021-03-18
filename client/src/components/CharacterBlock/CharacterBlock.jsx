3ef836f4f43922fb2cd3b47a77f052be0eb19cef
Christian Goldman <christian.goldman9638@gmail.com>
2021-03-18 15:46:04 -0500

    updating character name

--- client/src/components/CharacterBlock/CharacterBlock.jsx
+++ client/src/components/CharacterBlock/CharacterBlock.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import CharacterCreator from "../../pages/CharacterCreator";
+import React from "react";
 
 function CharacterBlock(props) {
   return (
     <div className="charBlock">
-      <p>Terry {props.selectedRace} {props.selectedClass}</p>
+      <p>
+        { props.charName } {props.selectedRace} {props.selectedClass}
+      </p>
       <hr />
       <p>Armor Class: 10</p>
       <p>Hit Points: 10</p>
       <hr />
-      <p>STR: <span>{props.strength}</span> DEX: <span>12</span> CON: <span>12</span> INT: <span>12</span> WIS: <span>12</span> CHA: <span>12</span></p>
+      <p>
+        STR:<span>{props.strength}</span>{" "} 
+        DEX:<span>{props.dexterity}</span>{" "} 
+        CON:<span>{props.constitution}</span>{" "}
+        INT:<span>{props.intelligence}</span>{" "}
+        WIS:<span>{props.wisdom}</span>{" "} 
+        CHA:<span>{props.charisma}</span>{" "} 
+      </p>
       <hr />
-      <p>Saving Throws: Str: <span>+2</span>, Con +2</p>
-      <p>Skills: <span>Athletics +2, Intimidation +2</span></p>
+      <p>
+        Saving Throws: Str: <span>+2</span>, Con +2
+      </p>
+      <p>
+        Skills: <span>Athletics +2, Intimidation +2</span>
+      </p>
       <hr />
-      <p>Equipment: <span></span></p>
-
+      <p>
+        Equipment: <span></span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default CharacterBlock
+export default CharacterBlock;
