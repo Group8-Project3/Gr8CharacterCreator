240abdc2cf3d1b8a2f40e60351447fbe4f9847d1
Jake <jakekippes123gmail.com>
2021-03-20 11:33:56 -0500

    added the passport setup, user model, and authentification routes.

---
+++ passport/setup.js
@@ -0,0 +1,55 @@
+const bcrypt = require("bcryptjs");
+const users = require("../models/users");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  users.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    users
+      .findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          const newUser = new users({ email, password });
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then((user) => {
+                  return done(null, user);
+                })
+                .catch((err) => {
+                  return done(null, false, { message: err });
+                });
+            });
+          });
+        } else {
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "Wrong password" });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        return done(null, false, { message: err });
+      });
+  })
+);
+
+module.exports = passport;
