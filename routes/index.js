0947e16754dbcab30d57ce1c3701b2e24bb35e6a
Jake <jakekippes123gmail.com>
2021-05-25 20:32:02 -0500

    fixing routes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+router.use( (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  });
 
 module.exports = router;
